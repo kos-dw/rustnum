98e4932c6b53322f18bab376ad64bde641d9e5b7
kos-dw <90103467+kos-dw@users.noreply.github.com>
2024-04-15 12:32:53 +0900

    fix(structs.rs): ドキュメンテーションコメントの削除
    実行ファイルのドキュメントに影響が出るため、構造体のコメントを削除

--- src/structs.rs
+++ src/structs.rs
@@ -1,16 +1,6 @@
 use clap::Parser;
 use sqlx::FromRow;
 
-/// 環境変数、及びコマンドライン引数をパースするための構造体
-///
-/// # Fields
-/// * `database` - データベースURL
-/// * `table` - テーブル名
-/// * `root` - ルートディレクトリ
-///
-/// # Note
-/// `Parser`を使用してコマンドライン引数をパースしています。
-///
 #[derive(Debug, Parser)]
 #[command(
     version,
@@ -36,20 +26,14 @@ pub struct Env {
     pub root: String,
 }
 
-///  テーブルのスキーマを表すための構造体
-///
-/// # Fields
-/// * `dir_id` - ディレクトリID
-/// * `dir_name` - ルートディレクトリ名
-/// * `current_number` - 現在の番号
-///
-/// # Note
-/// テーブルのスキーマを表すために`FromRow`を実装しています。  
-/// また、`dir_name`はディレクトリのパスではなく、ディレクトリ名を表します。
-///
 #[derive(FromRow, Debug)]
 pub struct Record {
+    /// Directory ID
     pub dir_id: i64,
+
+    /// Directory name
     pub dir_name: String,
+
+    /// Current number
     pub current_number: i64,
 }
